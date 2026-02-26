74db7c50d5be3a7e672a6d1e8bda297ae159d455
simontoy1978 <simontoy1978@gmail.com>
2026-02-26 14:15:07 +1100

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,73 @@
+variable "project_id" {
+type = string
+default = "terraform-487800"
+}
+
+variable "region" {
+type = string
+default = "us-west1"
+}
+
+variable "network_name" {
+type = string
+default = "app"
+}
+
+variable "network_IP_range" {
+type = string
+default = "10.20.0.0/16"
+}
+
+variable "image_project" {
+type = string
+default = "ubuntu-os-cloud"
+}
+
+variable "image_image" {
+type = string
+default = "ubuntu-2204-lts"
+}
+
+variable "machine_type" {
+type = string
+default = "e2-micro"
+}
+
+resource "google_compute_network" "app" {
+name        = "app"
+auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "app" {
+name          = "app"
+ip_cidr_range = "10.20.0.0/16"
+region        = "us-west1"
+network       = google_compute_network.app.id
+}
+
+
+data "google_compute_image" "ubuntu" {
+  most_recent = true
+  project     = "ubuntu-os-cloud" 
+  family      = "ubuntu-2204-lts"
+}
+
+resource "google_compute_instance" "blog" {
+  name         = "blog"
+  machine_type = "e2-micro"
+
+  
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.ubuntu.self_link
+    }
+  }
+  network_interface {
+   subnetwork = "app"
+   access_config {
+      # Leave empty for dynamic public IP
+    }
+  }
+
+allow_stopping_for_update = true
+}
